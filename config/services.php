54c3eb8f7e332df5ba652ae8167e79ae83732c66
Taisei <swimmer91122@gmail.com>
2025-10-02 09:42:07 +0900

    feat: Initial project setup

--- config/services.php
+++ config/services.php
@@ -14,6 +14,10 @@ return [
     |
     */
 
+    'openweathermap' => [
+        'key' => env('OPENWEATHERMAP_API_KEY'),
+    ],
+    
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
     ],
