801fe2691d399ea9752021b74c1916fd1811a7eb
codingTaisey <swimmer91122@gmail.com>
2025-10-21 15:23:00 +0900

    feat: 全体的なUI/UXの改善と、Chart.jsの導入

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,12 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import { Chart, registerables } from 'chart.js';
+
+// Chart.jsの全機能を登録
+Chart.register(...registerables);
 
 window.Alpine = Alpine;
+window.Chart = Chart;
 
 Alpine.start();
